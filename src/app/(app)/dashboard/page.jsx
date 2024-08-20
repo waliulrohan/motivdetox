583f2302f08574355d00d02f2452ce20a6bd2b9f
waliulohan <waliulislam1742@gmail.com>
2024-08-20 19:40:34 +0600

    todos

--- src/app/(app)/dashboard/page.jsx
+++ src/app/(app)/dashboard/page.jsx
@@ -2,9 +2,17 @@
 const page = () => {
     return (
         <div>
-            dashboard hello
+            
         </div>
     );
 };
+/*
+TODO:
+1. navigation(sidebar/navbar)
+2. animation(framer motion)
+3. design the overall structure
+4. ai implementation(chat)
+5. chat page
 
+*/
 export default page;
